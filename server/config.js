941dcc3a52e30fcc1c9bdfeb4a42535aa1a44d00
Linus Hernvall <linus.hernvall@gmail.com>
2023-08-02 17:33:46 +0200

    Setup DB and Server

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://linushernvall::QkcLk3ex0lWfRtDm@cluster0.x0hjonl.mongodb.net/ck-light?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
